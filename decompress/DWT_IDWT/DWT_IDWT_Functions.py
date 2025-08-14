04ddc4449ec386a7ce939085a7d5aa7ff844b16b
csuned <chuanhaosun.ed@gmail.com>
2025-08-14 19:58:15 +0800

    update readme

--- decompress/DWT_IDWT/DWT_IDWT_Functions.py
+++ decompress/DWT_IDWT/DWT_IDWT_Functions.py
@@ -1,13 +1,3 @@
-# Copyright (c) 2019, Adobe Inc. All rights reserved.
-#
-# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike
-# 4.0 International Public License. To view a copy of this license, visit
-# https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.
-
-"""
-自定义pytorch函数，实现一维、二维、三维张量的DWT和IDWT，未考虑边界延拓
-只有当图像行列数都是偶数，且重构滤波器组低频分量长度为2时，才能精确重构，否则在边界处有误差。
-"""
 import torch
 from torch.autograd import Function
 
